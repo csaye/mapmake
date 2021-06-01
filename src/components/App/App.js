99b1551ca018eff43a4985dd80a2fea7461ab679
Cooper Saye <coopersaye@gmail.com>
2021-06-01 12:22:18 -0700

    Initialize firebase.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,14 @@
 import Canvas from '../Canvas/Canvas.js';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+import { firebaseConfig } from '../../util/firebaseConfig.js';
+
 import './App.css';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
