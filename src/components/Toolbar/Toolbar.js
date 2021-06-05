2805f2b7ac4c71dbfd3dcc79a3e79e09e9514a3c
Cooper Saye <coopersaye@gmail.com>
2021-06-04 20:25:45 -0700

    Implement map name editing.

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,19 +1,42 @@
+import React, { useState } from 'react';
+
+import EditIcon from '@material-ui/icons/Edit';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import ImageIcon from '@material-ui/icons/Image';
 import DescriptionIcon from '@material-ui/icons/Description';
 import PublishIcon from '@material-ui/icons/Publish';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
+import Popup from 'reactjs-popup';
+
+import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore';
+
 import firebase from 'firebase/app';
 
 import './Toolbar.css';
 
 function Toolbar(props) {
+  const [error, setError] = useState('');
+  const [mapName, setMapName] = useState('');
+
+  const mapDoc = firebase.firestore().collection('maps').doc(props.map);
+  const [mapData] = useDocumentData(mapDoc);
+
+  // sets user current map to empty
   async function exitMap() {
     const uid = firebase.auth().currentUser.uid;
     const userDoc = firebase.firestore().collection('users').doc(uid);
     await userDoc.update({
       map: ''
+    });
+  }
+
+  // updates current map
+  async function updateMap() {
+    const newMapName = mapName;
+    setMapName('');
+    await mapDoc.update({
+      name: newMapName
     });
   }
 
@@ -69,6 +92,38 @@ function Toolbar(props) {
       <button onClick={() => exitMap()}>
         <ExitToAppIcon />
       </button>
+      <Popup
+        trigger={
+          <button className="editmap-btn">
+            <EditIcon />
+          </button>
+        }
+        modal
+      >
+        {
+          close => (
+            <div className="modal">
+              <button className="close" onClick={close}>&times;</button>
+              <h1>Editing {mapData ? mapData.name : '...'}</h1>
+              <form
+                onSubmit={e => {
+                  e.preventDefault();
+                  updateMap();
+                  close();
+                }}
+              >
+                <input
+                  placeholder="map name"
+                  value={mapName}
+                  onChange={e => setMapName(e.target.value)}
+                  required
+                />
+                <button>Update Map</button>
+              </form>
+            </div>
+          )
+        }
+      </Popup>
     </div>
   );
 }
