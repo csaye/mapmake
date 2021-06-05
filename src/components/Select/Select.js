65a2293e75445963311da42796ebd574bfa2808b
Cooper Saye <coopersaye@gmail.com>
2021-06-04 18:07:58 -0700

    Implement map creation and selection.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -21,6 +21,37 @@ function Select() {
   const userRef = usersRef.doc(uid);
   const [userData] = useDocumentData(userRef);
 
+  async function selectMap(map) {
+    await userRef.update({
+      map: map.id
+    });
+  }
+
+  async function createMap(e) {
+    e.preventDefault();
+    await mapsRef.add({
+      name: mapName,
+      users: [uid]
+    }).then(doc => {
+      const dataCollection = doc.collection('data');
+      dataCollection.doc('map').set({
+        tiles: '-'.repeat(32 * 32)
+      });
+      dataCollection.doc('images').set({
+        tile0: '',
+        tile1: '',
+        tile2: '',
+        tile3: '',
+        tile4: '',
+        tile5: '',
+        tile6: '',
+        tile7: '',
+        tile8: '',
+        tile9: ''
+      });
+    });
+  }
+
   if (!userData) return (
     <div className="Select">
       <p>Loading...</p>
@@ -32,6 +63,25 @@ function Select() {
       {
         userData.map ?
         <Canvas map={userData.map} /> :
+        maps ?
+        <>
+          {
+            maps.map((m, i) =>
+              <button key={`selectmap-${i}`} onClick={() => selectMap(m)}>
+                {m.name}
+              </button>
+            )
+          }
+          <form onSubmit={createMap}>
+            <input
+              value={mapName}
+              onChange={e => setMapName(e.target.value)}
+              required
+            />
+            <button>Create Map</button>
+          </form>
+        </> :
+        <p>Loading...</p>
       }
     </div>
   );
